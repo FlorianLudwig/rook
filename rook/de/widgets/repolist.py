681a0578dfd39cd7ae6b4e799415cbca9ddb7afe
Florian Ludwig <f.ludwig@greyrook.com>
2012-06-30 18:10:30 +0200

    Test commit

--- rook/de/widgets/repolist.py
+++ rook/de/widgets/repolist.py
@@ -2,6 +2,12 @@ from rw import www
 
 class Repolist(www.Widget):
     def render(self):
+        # get info about accessable repository
+        # ssh git@leijuna.de info
+        
+        # list of checked out repositories
+        os.listdir(os.environ['VIRTUAL_ENV'] + '/src/')
+        
         self.finsh(template='<b>repository list</b>')
 
 
