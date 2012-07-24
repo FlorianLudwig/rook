542e1d4e06b593689631b739e1ce25bf9501db48
Simon Pizonka <simon.pizonka@gmail.com>
2012-07-24 15:18:07 +0200

    Added color, clean up.

--- rook/repo_check.py
+++ rook/repo_check.py
@@ -5,42 +5,48 @@ from git import Repo, InvalidGitRepositoryError
 import os, sys, time
 
 
-def git_status(dir):
+def is_git_repo(dir):
+    if os.path.isdir(dir + '/.git'):
+        return True
+    else:
+        return False
+
+def check_dirs(dir):
     dirs = get_dirs_with_fullpath(dir)
     for full_dir in dirs:
-        try:
-            repo = Repo(full_dir)
-        except InvalidGitRepositoryError:
-            print "Ignore", full_dir
-            continue
+        git_status(full_dir)
 
-        for remote in repo.remotes:
-            remote.fetch()
+def git_status(dir):
+    dir = os.path.realpath(dir)
+    try:
+        repo = Repo(dir)
+    except InvalidGitRepositoryError:
+        print "Ignore", dir
+        return
 
-        print "Dir:", full_dir.split('/')[-1]
-        print "Is dirty:", repo.is_dirty()
+    for remote in repo.remotes:
+        remote.fetch()
 
-#        if len(repo.untracked_files) > 0:
-#            print "Untracked files:"
-#            for file in repo.untracked_files:
-#                print file, ";",
+    title = dir.split('/')[-1]
+    if repo.is_dirty():
+        title += '*'
 
-        commits_origin = Set(repo.iter_commits('origin/master'))
-        commits_local = Set(repo.iter_commits())
+    title += ' ' + ' '.join(green(branch.name) if branch == repo.active_branch else branch.name
+                     for branch in repo.branches) 
+    print bold(title)
+    print ""
 
-        push_commits = commits_local.difference(commits_origin)
-        if len(push_commits) > 0:
-            print "Commits to push (" + str(len(push_commits)) + "):"
-            print_commits(push_commits)
-        pull_commits = commits_origin.difference(commits_local)
-        if len(pull_commits) > 0:
-            print "Commits to pull (" + str(len(pull_commits)) + "):"
-            print_commits(pull_commits)
+    commits_origin = Set(repo.iter_commits('origin/master'))
+    commits_local = Set(repo.iter_commits())
 
-    #        for commit in list(repo.iter_commits('origin/master')):
-    #                       print commit
-
-    #               print "since", repo.commits_since()
+    push_commits = commits_local.difference(commits_origin)
+    if len(push_commits) > 0:
+        print red("Commits to push (" + str(len(push_commits)) + "):")
+        print_commits(push_commits)
+    pull_commits = commits_origin.difference(commits_local)
+    if len(pull_commits) > 0:
+        print red("Commits to pull (" + str(len(pull_commits)) + "):")
+        print_commits(pull_commits)
 
 
 def print_commits(commits):
@@ -52,13 +58,27 @@ def print_commits(commits):
 def get_dirs_with_fullpath(dir):
     return [os.path.join(dir, f) for f in os.listdir(dir) if os.path.isdir(os.path.join(dir, f))]
 
+def color(t, c):
+    return chr(0x1b)+"["+str(c)+"m"+t+chr(0x1b)+"[0m"
+
+def red(t):
+    return color(t, 31)
+
+def green(t):
+    return color(t, 32)
+
+def bold(t):
+    return color(t, 1)
 
 def main():
-    if len(sys.argv) == 1:
-        git_status('.')
-    else:
-        git_status(sys.argv[1])
+    dir = '.'
+    if len(sys.argv) > 1:
+        dir = sys.argv[1]
 
+    if is_git_repo(dir):
+        git_status(dir)
+    else:
+        check_dirs(dir)            
 
 if __name__ == '__main__':
     main()
