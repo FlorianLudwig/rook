f390b42c049dfe53a1bb61dbd3601c26092f5365
Florian Ludwig <f.ludwig@greyrook.com>
2012-10-31 15:27:50 +0100

    deploy script update

--- rook/deploy.py
+++ rook/deploy.py
@@ -18,6 +18,7 @@ def main():
     src = 'master'
     dst = 'staging'
     log = ''
+    real = 'real' in sys.argv
     for folder in os.listdir(env_dir):
         path = env_dir + folder
         try:
@@ -32,30 +33,38 @@ def main():
         proc.wait()
         log += proc.stdout.read()
         sp.Popen(['git', 'checkout', dst], cwd=os.path.join(env_dir, folder)).wait()
-        sp.Popen(['git', 'merge', src], cwd=os.path.join(env_dir, folder)).wait()
-        sp.Popen(['git', 'push'], cwd=os.path.join(env_dir, folder)).wait()
-    #webbrowser.open('https://docs.google.com/a/greyrook.com/document/d/1ilHY9Gjb-zFK_1Teh3DEGiinMfK6MlWnLNRguBfjN68/edit')
-    #open('/tmp/rdeploy.log', 'w').write(log)
-    #sp.Popen(['gedit', '/tmp/rdeploy.log'])
+        if real:
+            sp.Popen(['git', 'merge', src], cwd=os.path.join(env_dir, folder)).wait()
+            sp.Popen(['git', 'push'], cwd=os.path.join(env_dir, folder)).wait()
+    webbrowser.open('https://docs.google.com/a/greyrook.com/document/d/1ilHY9Gjb-zFK_1Teh3DEGiinMfK6MlWnLNRguBfjN68/edit')
+    open('/tmp/rdeploy.log', 'w').write(log)
+    sp.Popen(['gedit', '/tmp/rdeploy.log'])
 
-    print '-- compile client --'
-    os.chdir(os.path.join(env_dir, 'leijuna-client'))
-    #sp.Popen(['./compile-all-for-client.sh']).wait()
-    sp.Popen(['rsync', '-av', 'bin', 'root@leijuna.de:/srv/leijuna/deploy/staging/src/leijuna-server/leijuna/static/']).wait()
-    sp.Popen(['ssh', 'root@leijuna.de', '/srv/leijuna/deploy/staging/src/update']).wait()
-    sp.Popen(['ssh', '-t', 'root@leijuna.de', 'screen', '-r', 'eva']).wait()
+    if not real:
+        print 'not really doing anything, use'
+        print 'rdploy real'
+        print 'to actually change something on the server'
+    else:
+        print '-- compile client --'
+        os.chdir(os.path.join(env_dir, 'leijuna-client'))
+        # sp.Popen(['./compile-all-for-client.sh']).wait()
+        sp.Popen(['rsync', '-av', 'bin', 'root@leijuna.de:/srv/leijuna/deploy/staging/src/leijuna-server/leijuna/static/']).wait()
+        sp.Popen(['ssh', 'root@leijuna.de', '/srv/leijuna/deploy/staging/src/update']).wait()
 
 
-    print 'Done. checking out master again'
-    for folder in os.listdir(env_dir):
-        path = env_dir + folder
-        try:
-            repo = Repo(path)
-        except InvalidGitRepositoryError:
-            continue
-        if not dst in repo.branches:
-            continue
+        print 'Done. checking out master again'
+        for folder in os.listdir(env_dir):
+            path = env_dir + folder
+            try:
+                repo = Repo(path)
+            except InvalidGitRepositoryError:
+                continue
+            if not dst in repo.branches:
+                continue
         sp.Popen(['git', 'checkout', src], cwd=os.path.join(env_dir, folder)).wait()
+
+        print 'ssh into server'
+        sp.Popen(['ssh', '-t', 'root@leijuna.de', 'screen', '-r', 'eva']).wait()
     #parser = argparse.ArgumentParser(description=__doc__,
     #                                 formatter_class=argparse.RawTextHelpFormatter)
     #args = parser.parse_args()
