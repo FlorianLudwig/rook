8380099b47bb9211807f012535eb8cc00ea9e8a1
Florian Ludwig <f.ludwig@greyrook.com>
2014-01-27 12:28:33 +0100

    color fixes

--- rook/prompt.py
+++ rook/prompt.py
@@ -14,21 +14,31 @@ import cli
 
 def main():
     last_return_code = int(sys.argv[1])
+    bg = cli.white_bg if last_return_code == 0 else cli.red_bg
+    fg = cli.black if last_return_code == 0 else lambda x: x
+
     width = cli.terminal_size()[0]
-    user = pwd.getpwuid(os.getuid())[0]
-    if user == 'root':
-        user = cli.red(user)
+    uid = os.getuid()
+    user_name = pwd.getpwuid(uid)[0]
+    root = uid == 0
+    if root:
+        user = bg(cli.red(user_name)).encode('utf-8')
+    else:
+        user = bg(fg(user_name)).encode('utf-8')
     path = os.path.abspath('.').decode('utf-8')
-    text = u'%s@%s %s' % (user, socket.gethostname(), path)
+
+    text = u'@%s %s' % (socket.gethostname(), path)
 
     if len(text) < width:
-        text += ' ' * (width - len(text))
+        text += ' ' * (width - len(text) - len(user_name))
 
-    if last_return_code == 0:
-        print cli.white_bg(cli.black(text)).encode('utf-8')
+    title = path
+    sys.stdout.write('\033]0;' + title.encode('utf-8') + '\007')
+    print user + bg(fg(text)).encode('utf-8')
+    if root:
+        sys.stdout.write('# ')
     else:
-        print cli.red_bg(text)
-    sys.stdout.write('$ ')
+        sys.stdout.write('$ ')
 
 
 if __name__ == '__main__':
